1632596e0bd2cd9ff08947a7bbe6117b6ab71e89
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2022-01-12 11:59:53 +0530

    Update aboutme.js

--- js/aboutme.js
+++ js/aboutme.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
   });
 
   // smooth scrolling 
-/*
+
   $('a[href*="#"]').on('click',function(e){
 
     e.preventDefault();
@@ -36,4 +36,4 @@ $(document).ready(function(){
   });
 
 });
-*/
+
